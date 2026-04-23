2b9c6d39c6d1a249c6b43d4b8e0a031501cbe456
Oscar1623 <oscar.ramos@jala.university>
2026-04-23 12:14:46 -0600

    Delete the demo route for image uploader testing

--- pixpro-frontend/src/app/app.routes.ts
+++ pixpro-frontend/src/app/app.routes.ts
@@ -2,14 +2,6 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  // Temporary route for testing uploading images
-  {
-    path: 'test',
-    loadComponent: () => 
-      import('./test/features/upload-test/uploading-test').then(m => m.UploadTestComponent),
-    title: 'Prueba de imagenes'
-  },
-
   {
     path: '',
     redirectTo: 'login',
